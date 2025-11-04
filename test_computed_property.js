2108271da7ad208ff4ae0326b3348de01b5cb10c
Cameron Clark <cameron.clark@hey.com>
2025-11-04 12:09:15 +0000

    u

--- test_computed_property.js
+++ test_computed_property.js
@@ -1,2 +1,17 @@
-/** @deprecated */ const a = 'foo';
-const obj = { [a]: 'value' };
+// Test case from issue #226
+const FOO = 'foo';
+const BAR = 'bar';
+
+// This should not crash anymore with computed property names
+({ [FOO]: BAR } = {});
+
+// Additional test cases with computed properties
+const obj = {
+  [FOO]: function() {
+    return this;
+  },
+  [BAR]: () => {},
+};
+
+// Destructuring with computed property
+const result = ({ [FOO]: value } = { foo: 'test' });
