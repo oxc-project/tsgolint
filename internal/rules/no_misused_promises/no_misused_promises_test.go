f33dd3f07a2c5f5b18e30410be82cf40d4bb5803
no-yan <63000297+no-yan@users.noreply.github.com>
2026-02-06 03:14:53 +0900

    style: format code by `just fmt` (#625)

--- internal/rules/no_misused_promises/no_misused_promises_test.go
+++ internal/rules/no_misused_promises/no_misused_promises_test.go
@@ -1070,7 +1070,7 @@ declare const useCallback: <T extends (...args: unknown[]) => unknown>(
 ) => T;
 useCallback<ReturnsVoid | ReturnsPromiseVoid>(async () => {});
     `},
-	{Code: `if (process.env.SKIP) { return x; }`},
+		{Code: `if (process.env.SKIP) { return x; }`},
 	}, []rule_tester.InvalidTestCase{
 		{
 			Code: `
