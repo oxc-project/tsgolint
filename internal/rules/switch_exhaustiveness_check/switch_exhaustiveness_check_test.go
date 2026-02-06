61ee27a786087ee108bb6006256d975d49788709
no-yan <63000297+no-yan@users.noreply.github.com>
2026-02-06 19:24:38 +0900

    perf(test): use t.Parallel for all tests

--- internal/rules/switch_exhaustiveness_check/switch_exhaustiveness_check_test.go
+++ internal/rules/switch_exhaustiveness_check/switch_exhaustiveness_check_test.go
@@ -8,6 +8,7 @@ import (
 )
 
 func TestSwitchExhaustivenessCheckRule(t *testing.T) {
+	t.Parallel()
 	rule_tester.RunRuleTester(fixtures.GetRootDir(), "tsconfig.minimal.json", t, &SwitchExhaustivenessCheckRule, []rule_tester.ValidTestCase{
 		{Code: `
 type Day =
