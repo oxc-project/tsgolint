ef099e81f41f0e0dfc6fdc2acab1b8a14072a0c8
camchenry <1514176+camchenry@users.noreply.github.com>
2026-02-21 11:52:23 +0000

    feat: add suggestions for `switch-exhaustiveness-check` (#720)
    - Part of https://github.com/oxc-project/tsgolint/issues/668
    
    NOTE: AI was used to help implement much of this. I recommend reviewing this PR with whitespace changes hidden, as most of the changes revolve around simply extracting the helper closures to the top-level, since they don't need to change between rule runs.
    
    The ported code was strongly based on the upstream typescript-eslint implementation, and I believe we have parity with all of the test cases here.

--- internal/rules/switch_exhaustiveness_check/switch_exhaustiveness_check.go
+++ internal/rules/switch_exhaustiveness_check/switch_exhaustiveness_check.go
@@ -1,21 +1,24 @@
 package switch_exhaustiveness_check
 
 import (
+	"fmt"
 	"slices"
 	"strings"
 
 	"github.com/microsoft/typescript-go/shim/ast"
 	"github.com/microsoft/typescript-go/shim/checker"
+	"github.com/microsoft/typescript-go/shim/core"
+	"github.com/microsoft/typescript-go/shim/scanner"
 	"github.com/typescript-eslint/tsgolint/internal/rule"
 	"github.com/typescript-eslint/tsgolint/internal/utils"
 )
 
-// func buildAddMissingCasesMessage() rule.RuleMessage {
-// return rule.RuleMessage{
-// Id:          "addMissingCases",
-// Description: "Add branches for missing cases.",
-// }
-// }
+func buildAddMissingCasesMessage() rule.RuleMessage {
+	return rule.RuleMessage{
+		Id:          "addMissingCases",
+		Description: "Add branches for missing cases.",
+	}
+}
 
 func buildDangerousDefaultCaseMessage() rule.RuleMessage {
 	return rule.RuleMessage{
@@ -35,93 +38,214 @@ type SwitchMetadata struct {
 	// nil if there is no default case
 	DefaultCase               *ast.CaseOrDefaultClause
 	MissingLiteralBranchTypes []*checker.Type
-	// TODO: add support for fixed (symbolname is used only for fixes)
-	// SymbolName string
+	SymbolName                string
+}
+
+func isLiteralLikeType(t *checker.Type) bool {
+	return utils.IsTypeFlagSet(
+		t,
+		checker.TypeFlagsLiteral|checker.TypeFlagsUndefined|checker.TypeFlagsNull|checker.TypeFlagsUniqueESSymbol,
+	)
+}
+
+/**
+ * For example:
+ *
+ * - `"foo" | "bar"` is a type with all literal types.
+ * - `"foo" | number` is a type that contains non-literal types.
+ * - `"foo" & { bar: 1 }` is a type that contains non-literal types.
+ *
+ * Default cases are never superfluous in switches with non-literal types.
+ */
+func doesTypeContainNonLiteralType(t *checker.Type) bool {
+	return utils.Some(
+		utils.UnionTypeParts(t),
+		func(t *checker.Type) bool {
+			return utils.Every(
+				utils.IntersectionTypeParts(t),
+				func(t *checker.Type) bool {
+					return !isLiteralLikeType(t)
+				},
+			)
+		},
+	)
+}
+
+func getSwitchMetadata(typeChecker *checker.Checker, node *ast.SwitchStatement) *SwitchMetadata {
+	cases := node.CaseBlock.AsCaseBlock().Clauses.Nodes
+	defaultCaseIndex := slices.IndexFunc(cases, func(clause *ast.Node) bool {
+		return clause.Kind == ast.KindDefaultClause
+	})
+	var defaultCase *ast.CaseOrDefaultClause
+	if defaultCaseIndex > -1 {
+		defaultCase = cases[defaultCaseIndex].AsCaseOrDefaultClause()
+	}
+
+	discriminantType := utils.GetConstrainedTypeAtLocation(typeChecker, node.Expression)
+	symbolName := ""
+	if discriminantType.Symbol() != nil {
+		symbolName = discriminantType.Symbol().Name
+	}
+
+	caseTypeSet := make(map[*checker.Type]struct{}, len(cases))
+	hasUndefinedCase := false
+	for _, c := range cases {
+		if c.Kind == ast.KindDefaultClause {
+			continue
+		}
+
+		caseType := utils.GetConstrainedTypeAtLocation(typeChecker, c.AsCaseOrDefaultClause().Expression)
+		caseTypeSet[caseType] = struct{}{}
+		if utils.IsTypeFlagSet(caseType, checker.TypeFlagsUndefined) {
+			hasUndefinedCase = true
+		}
+	}
+
+	containsNonLiteralType := doesTypeContainNonLiteralType(discriminantType)
+
+	missingLiteralBranchTypes := make([]*checker.Type, 0, 10)
+	utils.TypeRecurser(discriminantType, func(t *checker.Type) bool {
+		if _, ok := caseTypeSet[t]; ok || !isLiteralLikeType(t) {
+			return false
+		}
+
+		// "missing", "optional" and "undefined" types are different runtime objects,
+		// but all of them have TypeFlags.Undefined type flag
+		if hasUndefinedCase && utils.IsTypeFlagSet(t, checker.TypeFlagsUndefined) {
+			return false
+		}
+
+		missingLiteralBranchTypes = append(missingLiteralBranchTypes, t)
+
+		return false
+	})
+
+	return &SwitchMetadata{
+		ContainsNonLiteralType:    containsNonLiteralType,
+		DefaultCase:               defaultCase,
+		MissingLiteralBranchTypes: missingLiteralBranchTypes,
+		SymbolName:                symbolName,
+	}
+}
+
+func requiresQuoting(text string) bool {
+	return !scanner.IsIdentifierText(text, core.LanguageVariantStandard)
+}
+
+func getNodeIndent(sourceFile *ast.SourceFile, node *ast.Node) string {
+	trimmed := utils.TrimNodeTextRange(sourceFile, node)
+	_, column := scanner.GetECMALineAndCharacterOfPosition(sourceFile, trimmed.Pos())
+	if column <= 0 {
+		return ""
+	}
+	return strings.Repeat(" ", column)
+}
+
+func buildCaseTest(typeChecker *checker.Checker, missingBranchType *checker.Type, symbolName string) string {
+	missingBranchName := ""
+	if missingBranchType.Symbol() != nil {
+		missingBranchName = missingBranchType.Symbol().Name
+	}
+
+	caseTest := ""
+	if utils.IsTypeFlagSet(missingBranchType, checker.TypeFlagsESSymbolLike) {
+		caseTest = missingBranchName
+	} else {
+		caseTest = typeChecker.TypeToString(missingBranchType)
+	}
+
+	if symbolName != "" && requiresQuoting(missingBranchName) {
+		escapedBranchName := strings.NewReplacer(
+			"'", "\\'",
+			"\n", "\\n",
+			"\r", "\\r",
+		).Replace(missingBranchName)
+
+		return fmt.Sprintf("%s['%s']", symbolName, escapedBranchName)
+	}
+
+	return caseTest
+}
+
+func buildMissingCaseLine(typeChecker *checker.Checker, missingBranchType *checker.Type, symbolName string) string {
+	if missingBranchType == nil {
+		return "default: { throw new Error('default case') }"
+	}
+
+	caseTest := buildCaseTest(typeChecker, missingBranchType, symbolName)
+	escapedCase := strings.NewReplacer("\\", "\\\\", "'", "\\'").Replace(caseTest)
+	return fmt.Sprintf("case %s: { throw new Error('Not implemented yet: %s case') }", caseTest, escapedCase)
+}
+
+func buildMissingCases(typeChecker *checker.Checker, missingBranchTypes []*checker.Type, symbolName string) []string {
+	missingCases := make([]string, 0, len(missingBranchTypes))
+	for _, missingBranchType := range missingBranchTypes {
+		missingCases = append(missingCases, buildMissingCaseLine(typeChecker, missingBranchType, symbolName))
+	}
+	return missingCases
+}
+
+func applyMissingCases(sourceFile *ast.SourceFile, node *ast.SwitchStatement, defaultCase *ast.CaseOrDefaultClause, missingCases []string) []rule.RuleFix {
+	cases := node.CaseBlock.AsCaseBlock().Clauses.Nodes
+
+	var lastCase *ast.Node
+	if len(cases) > 0 {
+		lastCase = cases[len(cases)-1]
+	}
+
+	caseIndent := ""
+	if lastCase != nil {
+		caseIndent = getNodeIndent(sourceFile, lastCase)
+	} else {
+		caseIndent = getNodeIndent(sourceFile, &node.Node)
+	}
+
+	fixString := ""
+	if len(missingCases) > 0 {
+		indented := make([]string, 0, len(missingCases))
+		for _, code := range missingCases {
+			indented = append(indented, caseIndent+code)
+		}
+		fixString = strings.Join(indented, "\n")
+	}
+
+	if lastCase != nil {
+		if defaultCase != nil {
+			beforeFixBuilder := strings.Builder{}
+			for _, code := range missingCases {
+				beforeFixBuilder.WriteString(code)
+				beforeFixBuilder.WriteByte('\n')
+				beforeFixBuilder.WriteString(caseIndent)
+			}
+
+			return []rule.RuleFix{
+				rule.RuleFixInsertBefore(sourceFile, &defaultCase.Node, beforeFixBuilder.String()),
+			}
+		}
+
+		return []rule.RuleFix{
+			rule.RuleFixInsertAfter(lastCase, "\n"+fixString),
+		}
+	}
+
+	return []rule.RuleFix{
+		rule.RuleFixReplace(
+			sourceFile,
+			node.CaseBlock,
+			strings.Join([]string{"{", fixString, caseIndent + "}"}, "\n"),
+		),
+	}
+}
+
+func fixSwitch(sourceFile *ast.SourceFile, typeChecker *checker.Checker, node *ast.SwitchStatement, missingBranchTypes []*checker.Type, defaultCase *ast.CaseOrDefaultClause, symbolName string) []rule.RuleFix {
+	missingCases := buildMissingCases(typeChecker, missingBranchTypes, symbolName)
+	return applyMissingCases(sourceFile, node, defaultCase, missingCases)
 }
 
 var SwitchExhaustivenessCheckRule = rule.Rule{
 	Name: "switch-exhaustiveness-check",
 	Run: func(ctx rule.RuleContext, options any) rule.RuleListeners {
 		opts := utils.UnmarshalOptions[SwitchExhaustivenessCheckOptions](options, "switch-exhaustiveness-check")
-
-		isLiteralLikeType := func(t *checker.Type) bool {
-			return utils.IsTypeFlagSet(
-				t,
-				checker.TypeFlagsLiteral|checker.TypeFlagsUndefined|checker.TypeFlagsNull|checker.TypeFlagsUniqueESSymbol,
-			)
-		}
-
-		/**
-		 * For example:
-		 *
-		 * - `"foo" | "bar"` is a type with all literal types.
-		 * - `"foo" | number` is a type that contains non-literal types.
-		 * - `"foo" & { bar: 1 }` is a type that contains non-literal types.
-		 *
-		 * Default cases are never superfluous in switches with non-literal types.
-		 */
-		doesTypeContainNonLiteralType := func(t *checker.Type) bool {
-			return utils.Some(
-				utils.UnionTypeParts(t),
-				func(t *checker.Type) bool {
-					return utils.Every(
-						utils.IntersectionTypeParts(t),
-						func(t *checker.Type) bool {
-							return !isLiteralLikeType(t)
-						},
-					)
-				},
-			)
-		}
-
-		getSwitchMetadata := func(node *ast.SwitchStatement) *SwitchMetadata {
-			cases := node.CaseBlock.AsCaseBlock().Clauses.Nodes
-			defaultCaseIndex := slices.IndexFunc(cases, func(clause *ast.Node) bool {
-				return clause.Kind == ast.KindDefaultClause
-			})
-			var defaultCase *ast.CaseOrDefaultClause
-			if defaultCaseIndex > -1 {
-				defaultCase = cases[defaultCaseIndex].AsCaseOrDefaultClause()
-			}
-
-			discriminantType := utils.GetConstrainedTypeAtLocation(ctx.TypeChecker, node.Expression)
-
-			caseTypes := make([]*checker.Type, 0, len(cases))
-			for _, c := range cases {
-				if c.Kind == ast.KindDefaultClause {
-					continue
-				}
-
-				caseTypes = append(caseTypes, utils.GetConstrainedTypeAtLocation(ctx.TypeChecker, c.AsCaseOrDefaultClause().Expression))
-			}
-
-			containsNonLiteralType := doesTypeContainNonLiteralType(discriminantType)
-
-			missingLiteralBranchTypes := make([]*checker.Type, 0, 10)
-			utils.TypeRecurser(discriminantType, func(t *checker.Type) bool {
-				if slices.Contains(caseTypes, t) || !isLiteralLikeType(t) {
-					return false
-				}
-
-				// "missing", "optional" and "undefined" types are different runtime objects,
-				// but all of them have TypeFlags.Undefined type flag
-				if slices.ContainsFunc(caseTypes, func(t *checker.Type) bool {
-					return utils.IsTypeFlagSet(t, checker.TypeFlagsUndefined)
-				}) && utils.IsTypeFlagSet(t, checker.TypeFlagsUndefined) {
-					return false
-				}
-
-				missingLiteralBranchTypes = append(missingLiteralBranchTypes, t)
-
-				return false
-			})
-
-			return &SwitchMetadata{
-				ContainsNonLiteralType:    containsNonLiteralType,
-				DefaultCase:               defaultCase,
-				MissingLiteralBranchTypes: missingLiteralBranchTypes,
-			}
-		}
 
 		checkSwitchExhaustive := func(node *ast.SwitchStatement, switchMetadata *SwitchMetadata) {
 			// If considerDefaultExhaustiveForUnions is enabled, the presence of a default case
@@ -141,7 +265,12 @@ var SwitchExhaustivenessCheckRule = rule.Rule{
 					missingBranches = append(missingBranches, ctx.TypeChecker.TypeToString(missingType))
 				}
 
-				ctx.ReportNode(node.Expression, buildSwitchIsNotExhaustiveMessage(strings.Join(missingBranches, " | ")))
+				ctx.ReportNodeWithSuggestions(node.Expression, buildSwitchIsNotExhaustiveMessage(strings.Join(missingBranches, " | ")), func() []rule.RuleSuggestion {
+					return []rule.RuleSuggestion{{
+						Message:  buildAddMissingCasesMessage(),
+						FixesArr: fixSwitch(ctx.SourceFile, ctx.TypeChecker, node, switchMetadata.MissingLiteralBranchTypes, switchMetadata.DefaultCase, switchMetadata.SymbolName),
+					}}
+				})
 			}
 		}
 
@@ -162,8 +291,12 @@ var SwitchExhaustivenessCheckRule = rule.Rule{
 			}
 
 			if switchMetadata.ContainsNonLiteralType && switchMetadata.DefaultCase == nil {
-				ctx.ReportNode(node.Expression, buildSwitchIsNotExhaustiveMessage("default"))
-				// TODO(port): missing suggestion
+				ctx.ReportNodeWithSuggestions(node.Expression, buildSwitchIsNotExhaustiveMessage("default"), func() []rule.RuleSuggestion {
+					return []rule.RuleSuggestion{{
+						Message:  buildAddMissingCasesMessage(),
+						FixesArr: fixSwitch(ctx.SourceFile, ctx.TypeChecker, node, []*checker.Type{nil}, switchMetadata.DefaultCase, switchMetadata.SymbolName),
+					}}
+				})
 			}
 		}
 
@@ -172,7 +305,7 @@ var SwitchExhaustivenessCheckRule = rule.Rule{
 
 				stmt := node.AsSwitchStatement()
 
-				metadata := getSwitchMetadata(stmt)
+				metadata := getSwitchMetadata(ctx.TypeChecker, stmt)
 				checkSwitchExhaustive(stmt, metadata)
 				checkSwitchUnnecessaryDefaultCase(metadata)
 				checkSwitchNoUnionDefaultCase(stmt, metadata)
