014723b5e9e56ee2300d98413712467a3957d734
Kalven Schraut <kalvens@rtvision.com>
2026-02-21 19:51:18 -0600

    perf: add more checks to avoid expensive function calls when not needed

--- internal/rules/no_unnecessary_type_arguments/no_unnecessary_type_arguments.go
+++ internal/rules/no_unnecessary_type_arguments/no_unnecessary_type_arguments.go
@@ -129,7 +129,7 @@ var NoUnnecessaryTypeArgumentsRule = rule.Rule{
 			if utils.IsIntrinsicErrorType(argType) {
 				return
 			}
-			if argType != paramType && (utils.IsTypeAnyType(argType) || utils.IsTypeAnyType(paramType) || !(checker.Checker_isTypeStrictSubtypeOf(ctx.TypeChecker, argType, paramType) && checker.Checker_isTypeStrictSubtypeOf(ctx.TypeChecker, paramType, argType))) {
+			if argType != paramType && (utils.IsTypeAnyType(argType) || utils.IsTypeAnyType(paramType) || (argType.Flags() != paramType.Flags()) || !checker.Checker_isTypeIdenticalTo(ctx.TypeChecker, argType, paramType)) {
 				return
 			}
 
