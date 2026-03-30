ef82786e1c40c676e99c891c10b3537f7592457f
camc314 <18101008+camc314@users.noreply.github.com>
2026-03-30 19:09:53 +0000

    feat(await-thenable): port promise aggregator checks and upstream tests (#851)
    fixes https://github.com/oxc-project/oxc/issues/20880

--- internal/rules/await_thenable/await_thenable.go
+++ internal/rules/await_thenable/await_thenable.go
@@ -1,7 +1,10 @@
 package await_thenable
 
 import (
+	"slices"
+
 	"github.com/microsoft/typescript-go/shim/ast"
+	"github.com/microsoft/typescript-go/shim/checker"
 	"github.com/microsoft/typescript-go/shim/scanner"
 	"github.com/typescript-eslint/tsgolint/internal/rule"
 	"github.com/typescript-eslint/tsgolint/internal/utils"
@@ -42,6 +45,20 @@ func buildAwaitUsingOfNonAsyncDisposableMessage() rule.RuleMessage {
 	}
 }
 
+func buildInvalidPromiseAggregatorInputMessage() rule.RuleMessage {
+	return rule.RuleMessage{
+		Id:          "invalidPromiseAggregatorInput",
+		Description: "Unexpected iterable of non-Promise (non-\"Thenable\") values passed to promise aggregator.",
+	}
+}
+
+var promiseAggregatorMethods = []string{
+	"all",
+	"allSettled",
+	"any",
+	"race",
+}
+
 var AwaitThenableRule = rule.Rule{
 	Name: "await-thenable",
 	Run: func(ctx rule.RuleContext, options any) rule.RuleListeners {
@@ -61,6 +78,41 @@ var AwaitThenableRule = rule.Rule{
 							},
 						}}
 					})
+				}
+			},
+			ast.KindCallExpression: func(node *ast.Node) {
+				expr := node.AsCallExpression()
+				if !isPromiseAggregatorMethod(ctx, expr) {
+					return
+				}
+
+				if len(expr.Arguments.Nodes) == 0 {
+					return
+				}
+
+				argument := expr.Arguments.Nodes[0]
+				if argument == nil {
+					return
+				}
+
+				if ast.IsArrayLiteralExpression(argument) {
+					for _, element := range argument.AsArrayLiteralExpression().Elements.Nodes {
+						if element == nil || ast.IsOmittedExpression(element) {
+							continue
+						}
+
+						t := utils.GetConstrainedTypeAtLocation(ctx.TypeChecker, element)
+						if isAlwaysNonAwaitableType(ctx.TypeChecker, element, t) {
+							ctx.ReportNode(element, buildInvalidPromiseAggregatorInputMessage())
+						}
+					}
+
+					return
+				}
+
+				t := utils.GetConstrainedTypeAtLocation(ctx.TypeChecker, argument)
+				if isInvalidPromiseAggregatorInput(ctx.TypeChecker, argument, t) {
+					ctx.ReportNode(argument, buildInvalidPromiseAggregatorInputMessage())
 				}
 			},
 			ast.KindForOfStatement: func(node *ast.Node) {
@@ -136,4 +188,95 @@ var AwaitThenableRule = rule.Rule{
 			},
 		}
 	},
+}
+
+func isPromiseAggregatorMethod(
+	ctx rule.RuleContext,
+	callExpression *ast.CallExpression,
+) bool {
+	callee := ast.SkipParentheses(callExpression.Expression)
+	if !ast.IsAccessExpression(callee) {
+		return false
+	}
+
+	methodName, ok := checker.Checker_getAccessedPropertyName(ctx.TypeChecker, callee)
+	if !ok || !slices.Contains(promiseAggregatorMethods, methodName) {
+		return false
+	}
+
+	return utils.IsPromiseConstructorLike(
+		ctx.Program,
+		ctx.TypeChecker,
+		utils.GetConstrainedTypeAtLocation(ctx.TypeChecker, callee.Expression()),
+	)
+}
+
+func isInvalidPromiseAggregatorInput(
+	typeChecker *checker.Checker,
+	node *ast.Node,
+	t *checker.Type,
+) bool {
+	if !isIterable(typeChecker, t) {
+		return false
+	}
+
+	for _, part := range utils.UnionTypeParts(t) {
+		valueTypes := getValueTypesOfArrayLike(typeChecker, part)
+		for _, valueType := range valueTypes {
+			if containsNonAwaitableType(typeChecker, node, valueType) {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func getValueTypesOfArrayLike(
+	typeChecker *checker.Checker,
+	t *checker.Type,
+) []*checker.Type {
+	if checker.IsTupleType(t) {
+		return checker.Checker_getTypeArguments(typeChecker, t)
+	}
+
+	if numberIndexType := utils.GetNumberIndexType(typeChecker, t); numberIndexType != nil {
+		return []*checker.Type{numberIndexType}
+	}
+
+	typeArguments := checker.Checker_getTypeArguments(typeChecker, t)
+	if len(typeArguments) != 0 {
+		return typeArguments[:1]
+	}
+
+	return nil
+}
+
+func isAlwaysNonAwaitableType(
+	typeChecker *checker.Checker,
+	node *ast.Node,
+	t *checker.Type,
+) bool {
+	return utils.Every(utils.UnionTypeParts(t), func(typePart *checker.Type) bool {
+		return utils.NeedsToBeAwaited(typeChecker, node, typePart) == utils.TypeAwaitableNever
+	})
+}
+
+func containsNonAwaitableType(
+	typeChecker *checker.Checker,
+	node *ast.Node,
+	t *checker.Type,
+) bool {
+	return utils.Some(utils.UnionTypeParts(t), func(typePart *checker.Type) bool {
+		return utils.NeedsToBeAwaited(typeChecker, node, typePart) == utils.TypeAwaitableNever
+	})
+}
+
+func isIterable(
+	typeChecker *checker.Checker,
+	t *checker.Type,
+) bool {
+	return utils.Every(utils.UnionTypeParts(t), func(typePart *checker.Type) bool {
+		return utils.GetWellKnownSymbolPropertyOfType(typePart, "iterator", typeChecker) != nil
+	})
 }
